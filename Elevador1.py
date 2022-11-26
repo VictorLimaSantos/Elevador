1dac500585e854ee4afb56b46194de6677a46fdc
Victor <victorlimasantos.dev@gmail.com>
2022-11-25 23:33:26 -0300

    continuidade da codificação do aplicativo.

--- Elevador1.py
+++ Elevador1.py
@@ -7,7 +7,8 @@ pessoa_andar = {}
 pessoa_peso ={}
 
 pessoas = int (input("Quantas pessoas entraram no elevador? \n"))
-andares = 0
+andar = 0
+andares = []
 peso = 0
 peso_total = 0
 
@@ -16,14 +17,17 @@ for pessoa in range(1 , pessoas+1):
         print("exesso de passageiros")
 
     else:
-        andares = int(input(f"{pessoa}° pessoa, qual o andar desejado? "))
-        if andares < -2 or andares > 15:
+        andar = int(input(f"{pessoa}° pessoa, qual o andar desejado? "))
+        if andar < -2 or andar > 15:
             print("Andar invalido")
             continue
 
         else:
-            pessoa_andar[pessoa] = andares
+            pessoa_andar[pessoa] = andar
+            andares.append(andar)
+            andares.sort()
             print(pessoa_andar)
+            
 
             peso = float (input(f"Digite o Peso da {pessoa}° pessoa: "))
             peso_total = peso_total + peso
@@ -33,9 +37,14 @@ for pessoa in range(1 , pessoas+1):
             else:
                 pessoa_peso[pessoa] = peso
                 print(pessoa_peso)
-            index = 0
-            for andar in andares:
-                if peso_total <=1000 and pessoa <= 20:
-                    print(f" Elevador esta subindo para o andar {andares[index]}")
-                    index += 1
-                
+                index =0 
+                for andar in andares:
+                    if peso_total <=1000 or pessoa <= 20:
+                        print(f" Elevador esta subindo para o {andares[index]}° andar ")
+                        index += 1
+                    if pessoa_andar.get(andar):
+                        print(f"passageiros {pessoa_andar} chegaram ao andar {andares}")
+                    else:
+                        continue
+
+                        
